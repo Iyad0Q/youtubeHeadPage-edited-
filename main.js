8a4d7f01d0f71a77c65df0ff8e14ea18344d7cbb
Iyad Al-Qassab <110106501+Iyad0Q@users.noreply.github.com>
2023-04-15 21:05:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// vars
+let nav = document.querySelector("nav");
+let button = document.querySelector("#menu");
+let contatiner = document.querySelector(".videos-container");
+
+// functions
+button.addEventListener("click", function() {
+    nav.classList.toggle("active");
+    button.classList.toggle("active");
+    contatiner.classList.toggle("active");
+});
